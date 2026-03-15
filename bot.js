1ed789275179a8bd8269473e350198486887417e
emilypeterf-a11y <emilypeterf@icloud.com>
2026-03-15 14:29:29 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -84,6 +84,8 @@ app.post("/telegram", async (req, res) => {
   res.sendStatus(200);
 });
 
-app.listen(3000, () => {
-  console.log("Bot rodando");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Bot rodando na porta " + PORT);
 });
